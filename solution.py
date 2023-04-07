471f651db51ce8757757f8b3e81e13a445ef5514
ZRop <90032285+ZRop@users.noreply.github.com>
2023-04-07 17:43:30 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,14 +4,12 @@ import numpy as np
 from scipy.stats import norm
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 672508499 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
     alpha = 1 - p
-    
-    
-    return 1/(1-alpha), \
-           1/p
+    return (alpha*0.014 + 0.028 + x)/p, \
+           (p*0.014 + 0.028 + x)/(alpha)
